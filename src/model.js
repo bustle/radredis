9b28e4d58c0a9bb96b16540a429b9589132aceb9
Axel <axel@axelmolina.com>
2015-09-02 13:20:32 -0700

    lifecycle

--- src/model.js
+++ src/model.js
@@ -3,8 +3,22 @@ import Attribute from './model/attribute';
 
 class Model {
 
+  //class methods
+
+  static get redis() {
+    return Config.redis;
+  }
+
+  static get redisKey() {
+    return Config.redisKeyPrefix + this._redisKey;
+  }
+
   static get schema() {
     return this._schema;
+  }
+
+  static set redisKey(key) {
+    this._redisKey = key;
   }
 
   static set schema(schema) {
@@ -14,62 +28,79 @@ class Model {
     this._schema = schema;
   }
 
+  static find(id) {
+    return new Promise((resolve, reject) => {
+      let model = new this;
+      model.attributes.id = id;
+      model.load().then((model) => {
+        resolve(model);
+      }).catch(reject);
+    });
+  }
+
+  //instance methods
 
   constructor(attributes={}) {
-    let id = attributes.id || null;
-    if(id) { delete attributes.id; }
     this._attributes = attributes;
   }
 
-
-  get id() {
-    return this._id;
-  }
-
-  set id(value) {
-    this._id = value;
+  generateId() {
+    return this.redis.incr(this.constructor.redisKey + ':id');
   }
 
   get attributes() {
     return this._attributes;
   }
 
-
-  /* State & Flags */
-  get isDirty() {
-    return this._isDirty || false;
+  get id() {
+    return this.attributes.id;
   }
 
-  set isDirty(val) {
-    this._isDirty = val;
+  get redis() {
+    return this.constructor.redis;
   }
 
-  get isNew() {
-    return this.id || true;
+  get redisKey() {
+    let key = this.constructor.redisKey + ':' + this.id;
+    return key;
   }
 
+  create() {
+    return new Promise((resolve, reject) => {
+      this.generateId().then((id) => {
+        this.attributes.id = id;
+        this.update().then(resolve(this)).catch(reject);
+      }).catch(reject);
+    })
+  }
 
-  /* Lifecycle */
+  load() {
+    return new Promise((resolve, reject) => {
+      this.redis.hgetall(this.redisKey + ':_attributes').then((data) => {
+        this._attributes = data;
+        resolve(this);
+      }).catch(reject);
+    })
+  }
+
   save() {
-    return Promise.reject('implement');
+    if(this.id) {
+      return this.update();
+    } else {
+      return this.create();
+    }
   }
 
-  updateAttributes(attributes) {
-    return Promise.reject('implement');
+  update() {
+    return new Promise((resolve, reject) => {
+      this.redis.hmset(this.redisKey + ':_attributes', this.attributes).then(resolve(this)).catch(reject);
+    })
   }
 
   destroy() {
-    return Promise.reject('implement');
-  }
-
-
-  /* Helpers, getters, setters */
-  get redis() {
-    return this._redis || Config.redis;
-  }
-
-  set redis(connection) {
-    this._redis = connection;
+    return new Promise((resolve, reject) => {
+      this.redis.del(this.redisKey + ':_attributes').then(resolve(this)).catch(reject);
+    })
   }
 
 }
--- test/model.js
+++ test/model.js
@@ -2,6 +2,7 @@ import assert from 'assert';
 import Model from '../src/model';
 
 class Person extends Model {};
+Person.redisKey = 'person';
 Person.schema = [ 
   { key: 'name' },
   { key: 'birthday' },
@@ -10,20 +11,21 @@ Person.schema = [
 ];
 
 describe('Model', () => {
-  describe(".constructor()", () => {
-    let person, attributes;
+  let person, attributes;
 
-    beforeEach(() => {
-      attributes = { 
-        name: "Tyler Love",
-        birthday: Date.now()
-      };
-      person = new Person(attributes);
-    });
+  beforeEach(() => {
+    attributes = {
+      name: "John Smith",
+      birthday: Date.now()
+    };
+    person = new Person(attributes);
+  });
+
+  describe(".constructor()", () => {
 
     it("assigns attributes from the constructor", () => {
       assert.equal(person.name, attributes.name);
-      assert.equal(person.birthdate, attributes.birthdate);
+      assert.equal(person.birthday, attributes.birthday);
     });
 
     it("sets attributes", () => {
@@ -40,4 +42,64 @@ describe('Model', () => {
     });
 
   });
+
+  describe("#create()", () => {
+
+    it("persists new model", () => {
+      let attributes = person.attributes;
+      return person.create()
+        .then((p) => { return p.load(); })
+        .then((p) => {
+          assert.equal(p.name, attributes.name);
+        }).catch((err)=> {
+          assert.ifError(err);
+        });
+    });
+
+  });
+
+  describe("#update()", () => {
+
+    it("persists attributes", () => {
+      return person.create()
+      .then((p) => { p.name = 'Jane Doe'; return p.update(); })
+      .then((p) => { return p.load(); })
+      .then((p) => {
+        assert.equal(p.name, 'Jane Doe');
+      }).catch((err) => {
+        assert.ifError(err);
+      });        
+    });
+
+  });
+
+  describe("#destroy()", () => {
+
+    it("destroys record", () => {
+      return person.create()
+      .then((p) => { return p.destroy(); })
+      .then((p) => { return p.load(); })
+      .then((p) => {
+        assert.equal(p.name, null);
+      }).catch((err) => {
+        assert.ifError(err);
+      });        
+    });
+
+  });
+
+  describe("::find()", () => {
+
+    it("finds and loads record", () => {
+      return person.create()
+      .then((p) => { return Person.find(p.id); })
+      .then((p) => {
+        assert.equal(person.name, p.name);
+      }).catch((err) => {
+        assert.ifError(err);
+      });        
+    });
+
+  });
+
 });
