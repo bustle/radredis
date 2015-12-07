3d2ce9209b946ef4f293b85d0472aa766cf01fbd
Steve Faulkner <southpolesteve@gmail.com>
2015-12-07 12:50:48 -0500

    Add .scan method

---
+++ test/scan.js
@@ -0,0 +1,44 @@
+const radredis = require('../src')
+const flush = require('./flushdb')
+const redisOpts = require('./redis-opts')
+const expect = require('expect.js')
+const Promise = require('bluebird')
+
+const schema = {
+  title: 'Post',
+  properties: {
+    title: { type: 'string' },
+    author_id: { type: 'integer', index: true }
+  }
+}
+const Post = radredis(schema, {}, redisOpts)
+
+describe('Radredis', function() {
+
+  before(function(){
+    return flush().then(function(){
+      return Promise.all([
+        Post.create({title: 'test', author_id: 1 }),
+        Post.create({title: 'test', author_id: 2 }),
+        Post.create({title: 'test', author_id: 3 }),
+        Post.create({title: 'test', author_id: 3 })
+      ])
+    })
+  })
+
+  describe('.scan', function(){
+
+    it('should return all results', function(done){
+      const stream = Post.scan()
+      let count = 0
+      stream.on('data', (data) => {
+        expect(data).to.be.an(Object)
+        count++
+      })
+      stream.on('end', ()=>{
+        expect(count).to.eql(4)
+        done()
+      })
+    })
+  })
+});
